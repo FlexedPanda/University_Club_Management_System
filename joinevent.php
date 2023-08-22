47b52dc676061e2ee915ededee8d0b8d78f7f004
MD Ikramul Kayes <67923321+ikramulkayes@users.noreply.github.com>
2023-08-22 09:41:29 +0600

    updated joinevent.php
    fixed the bug with facing error while joining the same event twice

--- joinevent.php
+++ joinevent.php
@@ -4,26 +4,46 @@ $studentID = $_POST['studentID'];
 
 require_once('dbconnect.php');
 
-$sql = "INSERT INTO participate (member_id, event_id) VALUES ('$studentID', '$eventID')";
+// Check if the member is already participating in the event
+$isParticipatingQuery = "SELECT * FROM participate WHERE member_id = '$studentID' AND event_id = '$eventID'";
+$isParticipatingResult = mysqli_query($conn, $isParticipatingQuery);
 
-if (mysqli_query($conn, $sql)) {
+if (mysqli_num_rows($isParticipatingResult) > 0) {
+    // Member is already participating in the event
     echo '
     <head>
-        <title>Joined</title>
+        <title>Already Joined</title>
         <link rel="stylesheet" type="text/css" href="css/error.css">
     </head>
     <body background="img/bracubackground.jpg">
         <div class="center-div">
-            <h1>Event Joined<i></i></h1>
-            <p>You Have Successfully Joined The Event.</p>
-            <button class="btn" onclick="history.go(-1);" >Go Back To Homepage<i></i></button>
+            <h1>Event Already Joined<i></i></h1>
+            <p>You are already participating in this event.</p>
+            <button class="btn" onclick="history.go(-1);" >Go Back<i></i></button>
         </div>
     </body>
     ';
-} 
+} else {
+    // Member is not participating, so insert the participation record
+    $sql = "INSERT INTO participate (member_id, event_id) VALUES ('$studentID', '$eventID')";
 
-else {
-    echo "Error: " . mysqli_error($conn);
+    if (mysqli_query($conn, $sql)) {
+        echo '
+        <head>
+            <title>Joined</title>
+            <link rel="stylesheet" type="text/css" href="css/error.css">
+        </head>
+        <body background="img/bracubackground.jpg">
+            <div class="center-div">
+                <h1>Event Joined<i></i></h1>
+                <p>You Have Successfully Joined The Event.</p>
+                <button class="btn" onclick="history.go(-1);" >Go Back To Homepage<i></i></button>
+            </div>
+        </body>
+        ';
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
 }
 
 mysqli_close($conn);
