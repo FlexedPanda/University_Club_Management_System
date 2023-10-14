fb3bc52b9769e13224cac93796eab642c9921650
Umme Abira Azmary <95493187+Mouly22@users.noreply.github.com>
2023-10-14 13:12:43 +0600

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
-// Database connection parameters
-$hostname = 'db';        // Hostname (use the container name or IP)
-$username = 'php_docker'; // MySQL username
-$password = 'password';  // MySQL password
-$database = 'php_docker'; // Database name
+
+$hostname = 'db';      
+$username = 'php_docker';
+$password = 'password';  
+
 
 // Create a connection to the MySQL database
 $conn = mysqli_connect($hostname, $username, $password, $database);
